2a013c91be4bd4732242f1a1247a087d618d3eb4
mahalakshmi <chikkuboye@gmail.com>
2022-11-06 18:59:11 +0530

    adding

---
+++ Books.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Books {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int choice;
+        while (true){
+            System.out.println("Enter the option below");
+            System.out.println("1 :Insert");
+            System.out.println("2 :View");
+            System.out.println("3 :Search");
+            System.out.println("4 :Update");
+            System.out.println("5 :Delete");
+            System.out.println("6 :Exit");
+
+            choice = sc.nextInt();
+
+            switch (choice){
+                case 1:
+                    System.out.println("Insert");
+                    break;
+                case 2:
+                    System.out.println("View");
+                    break;
+                case 3:
+                    System.out.println("Search");
+                    break;
+                case 4:
+                    System.out.println("Update");
+                    break;
+                case 5:
+                    System.out.println("Delete");
+                    break;
+                case 6:
+                    System.exit(0);
+
+            }
+
+        }
+
+    }
+}
